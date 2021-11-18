7620e4ae0297c06cbfc44949a086d42d8c3ccb40
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-11-18 14:33:38 +0900

    scss 로 react에서 스타이링 작업하기

--- react-practice/src/App2.js
+++ react-practice/src/App2.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import './styles/App.scss';
 import Button from './component/Button';
 
 function App2() {
   return (
     <div className="App">
       <div className="button">
+        <Button size="large" color="pink">
+          Button
+        </Button>
         <Button>Button</Button>
+        <Button size="small" color="gray">
+          Button
+        </Button>
+      </div>
+      <div className="button">
+        <Button size="large" color="pink" outline={true} fullWidth={true}>
+          Button
+        </Button>
+        <Button>Button</Button>
+        <Button size="small" color="gray" outline>
+          Button
+        </Button>
       </div>
     </div>
   );
--- react-practice/src/component/Button.js
+++ react-practice/src/component/Button.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import classNames from 'classnames';
 import '../styles/Button.scss';
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+// size 종류에는 large, medium, small로 구분
+function Button({ children, size, color, outline, fullWidth }) {
+  return (
+    <button
+      className={classNames('Button', size, color, {
+        outline,
+        fullWidth,
+      })}
+    >
+      {children}
+    </button>
+  );
 }
 
+Button.defaultProps = {
+  size: 'medium',
+  color: 'blue',
+};
 export default Button;
